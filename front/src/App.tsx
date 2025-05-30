971753153bda24bfab9406b633e6c7ee20ae9b0e
potapchukdmytro <traiget@gmail.com>
2025-05-30 17:52:08 +0300

    create user

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,6 +5,8 @@ import RoleListPage from "./pages/role/RoleListPage";
 import UserListPage from "./pages/user/UserListPage";
 import CarListPage from "./pages/car/CarListPage";
 import CreateCarPage from "./pages/car/CreateCarPage";
+import UserCreatePage from "./pages/user/UserCreatePage";
+import UserUpdatePage from "./pages/user/UserUpdatePage";
 
 function App() {
     return (
@@ -13,10 +15,14 @@ function App() {
             <div style={{ width: "66%", margin: "30px auto" }}>
                 <Routes>
                     <Route path="/roles" element={<RoleListPage />} />
-                    <Route path="/users" element={<UserListPage />} />
+                    <Route path="/users">
+                        <Route index element={<UserListPage />} />
+                        <Route path="create" element={<UserCreatePage />} />
+                        <Route path="update/:id" element={<UserUpdatePage />} />
+                    </Route>
                     <Route path="/cars">
-                        <Route index element={<CarListPage/>}/>
-                        <Route path="create" element={<CreateCarPage/>}/>
+                        <Route index element={<CarListPage />} />
+                        <Route path="create" element={<CreateCarPage />} />
                     </Route>
                 </Routes>
             </div>
